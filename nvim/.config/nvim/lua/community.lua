c62bfc8b5a7ba290af1fe38459f3c22d8644b9af
Tushar Jagad <tushar.jagad@gmail.com>
2024-11-03 18:08:00 +0530

    Initial commit
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,42 @@
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  -- import/override with your plugins folder
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.bars-and-lines.scope-nvim" },
+  { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+
+  { import = "astrocommunity.color.headlines-nvim" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.marks-nvim" },
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+
+  --  { import = "astrocommunity.utility.noice-nvim" },
+}
