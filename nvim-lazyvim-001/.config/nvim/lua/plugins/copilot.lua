c78550fe2e65e96cd80dca0ad8337e29c8562875
Tushar Jagad <tushar.jagad@gmail.com>
2025-03-24 19:03:58 +0530

    nvim: add configurations based on kickstart.nvim and lazyvim.nvim
    This patch add basic configurations for neovim based on kickstart.nvim
    and lazyvim.nvim.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

---
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      help = true,
+      gitcommit = true,
+      gitrebase = true,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = true,
+    },
+  },
+}
