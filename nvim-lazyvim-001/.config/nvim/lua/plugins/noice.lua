b7e5239597e44a15e3cd5bbcf314f93dac2ed263
Tushar Jagad <tushar.jagad@gmail.com>
2025-06-27 20:24:14 +0530

    nvim: nvim-lazyvim-001: set noice presets
    This commit overrides noice presets in LazyVim to
    - move search inline with command
    - add borders to lsp docs and signature help
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

---
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      bottom_search = false, -- use a classic bottom cmdline for search
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
+  },
+}
