e691e64b9f9cd9ee950cc06c34385738a3a74db9
Tushar Jagad <tushar.jagad@gmail.com>
2025-07-02 13:31:25 +0530

    nvim: nvim-lazyvim-001: move catppuccin customizations to a separate file
    Move customization related to catppuccin to a dedicated file. The
    colorscheme.lua is use to set the neovim colorscheme.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- nvim-lazyvim-001/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,6 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-      no_italic = true,
-      highlight_overrides = {
-        mocha = function(colors)
-          return {
-            WinSeparator = { fg = "#45475a" },
-            -- use soft green, no italics for comments
-            Comment = { fg = "#a6e3a1", style = {} },
-            -- override default Pmenu colors to match overrides above
-            Pmenu = { fg=colors.text, bg=colors.base },
-          }
-        end,
-      },
-      integrations = {
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-        blink_cmp = {
-          enabled = true,
-          style = 'bordered',
-        },
-      },
-    },
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin",
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  }
 }
