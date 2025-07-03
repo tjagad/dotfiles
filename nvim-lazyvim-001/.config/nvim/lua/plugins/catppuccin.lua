18f81fa3c8c3c76ecceca0e08327766b06f756ec
Tushar Jagad <tushar.jagad@gmail.com>
2025-07-03 10:52:58 +0530

    nvim: nvim-lazyvim-001: catppuccin: override highlight groups
    This commit updates the highlight groups so that information is
    displayed in a better way when using black background.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- nvim-lazyvim-001/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/catppuccin.lua
@@ -13,11 +13,17 @@
       highlight_overrides = {
         mocha = function(colors)
           return {
-            WinSeparator = { fg = "#45475a" },
+            WinSeparator = { fg = colors.surface1 },
             -- use soft green, no italics for comments
-            Comment = { fg = "#a6e3a1", style = {} },
+            Comment = { fg = colors.green, style = {} },
             -- override default Pmenu colors to match overrides above
-            Pmenu = { fg=colors.text, bg=colors.base },
+            Pmenu = { fg = colors.text, bg = colors.base },
+            -- use brighter colors for relative numbers in the signcolumn
+            LineNrAbove = { fg = colors.overlay1 },
+            LineNr = { fg = colors.overlay1 },
+            LineNrBelow = { fg = colors.overlay1 },
+            -- use brighter colors for line numbers in the signcolumn
+            CursorLineNr = { fg = colors.mauve },
           }
         end,
       },
