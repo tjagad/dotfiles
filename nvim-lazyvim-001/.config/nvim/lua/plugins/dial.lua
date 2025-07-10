5592c3e539770ebc972092858e2153922240c15d
Tushar Jagad <tushar.jagad@gmail.com>
2025-07-11 00:29:19 +0530

    nvim: nvim-lazyvim-001: remove dial customization to increment numbers
    The keybaord mappings for tmux have changed and the default shortcut for
    incrementing numbers can now be used without conflicts. This commit reverts
    the customized key mappings for incrementing numbers via the dial
    plugin.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- nvim-lazyvim-001/.config/nvim/lua/plugins/dial.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "monaqa/dial.nvim",
-  keys = {
-    { "<C-b>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment", mode = {"n", "v"} },
-    { "g<C-b>", function() return require("dial.map").inc_gnormal() end, expr = true, desc = "Increment", mode = {"n", "v"} },
-  },
-}
