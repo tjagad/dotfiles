f5bfcf99c9fcc29130eb39ea0c7c546de3bacc2f
Tushar Jagad <tushar.jagad@gmail.com>
2025-07-21 21:50:44 +0530

    wezterm: use MonacoLigaturized Nerd Font Mono
    This patch configures wezterm to use MonacoLigaturized Nerd Font Mono as
    the default font.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,9 +4,10 @@ local config = wezterm.config_builder()
 config.default_prog = { '/bin/bash' }
 
 config.font_size = 12
+config.font = wezterm.font('FiraCode Nerd Font Mono Ret')
 config.font = wezterm.font('CodeNewRoman Nerd Font Mono')
-config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
 config.font = wezterm.font('Maple Mono NF')
+config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
 config.enable_tab_bar = false
 
 config.colors = {
