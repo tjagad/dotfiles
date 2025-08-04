0070758a4277aa13a908c13913065c038bef5ec1
Tushar Jagad <tushar.jagad@gmail.com>
2025-08-04 18:21:22 +0530

    wezterm: use recursive mono linear font
    This patch configures wezterm to use RecMonoLinear Nerd Font Mono as the
    terminal font.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ config.font = wezterm.font('FiraCode Nerd Font Mono Ret')
 config.font = wezterm.font('CodeNewRoman Nerd Font Mono')
 config.font = wezterm.font('Maple Mono NF')
 config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
+config.font = wezterm.font('RecMonoLinear Nerd Font Mono')
 config.enable_tab_bar = false
 
 config.colors = {
