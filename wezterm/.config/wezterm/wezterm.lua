79e4e61cea41bd05bc45e92b57bbb8eca3268014
Tushar Jagad <tushar.jagad@gmail.com>
2025-08-20 09:40:28 +0530

    wezterm: use font_with_fallback() instead of font()
    This commit uses font_with_fallback() to set the font in wezterm
    configuration.
    
    It also builds on top of the font() configuration by capturing various
    fonts which are available in the system for easy switching.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ config.default_prog = { '/bin/bash' }
 config.freetype_load_flags = 'NO_HINTING'
 
 config.font_size = 12
+-- config.font = wezterm.font('RecMonoLinear Nerd Font Mono', { weight = 'Light', italic = true })
 config.font = wezterm.font('FiraCode Nerd Font Mono Ret')
 config.font = wezterm.font('CodeNewRoman Nerd Font Mono')
 config.font = wezterm.font('Maple Mono NF')
@@ -13,6 +14,14 @@ config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
 config.font = wezterm.font('RecMonoCasual Nerd Font Mono')
 config.font = wezterm.font('RecMonoLinear Nerd Font Mono')
 config.font = wezterm.font('RecMonoSmCasual Nerd Font Mono')
+config.font = wezterm.font('DroidSansM Nerd Font Mono')
+config.font = wezterm.font('VictorMono Nerd Font Mono', { weight = 'DemiBold' })
+config.font = wezterm.font('JuliaMono')
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono', { weight = 'DemiBold', stretch = 'Normal' })
+config.font = wezterm.font_with_fallback {
+  { family = 'Maple Mono', weight = 'DemiBold', stretch = 'Normal' },
+  { family = 'Symbols Nerd Font Mono', weight = 'Regular', stretch = 'Normal' }
+}
 config.enable_tab_bar = false
 
 config.colors = {
