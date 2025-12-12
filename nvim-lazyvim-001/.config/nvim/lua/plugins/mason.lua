83e197601e5745c4132bbafe02785960fe5d8634
Tushar Jagad <tushar.jagad@gmail.com>
2025-12-12 14:39:03 +0530

    nvim: add mason.nvim config to ensure consistent tool installation
    Add a plugins/mason.lua file to specify a list of tools to be
    installed via mason.nvim. This ensures all required language servers
    and formatters are present for development environments.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

---
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "asm-lsp",
+      "clangd",
+      "cmakelang",
+      "cmakelint",
+      "copilot-language-server",
+      "docker-compose-language-service",
+      "dockerfile-language-server",
+      "gopls",
+      "helm-ls",
+      "json-lsp",
+      "lua-language-server",
+      "marksman",
+      "neocmakelsp",
+      "pyright",
+      "ruff",
+      "taplo",
+      "yaml-language-server",
+    },
+  },
+}
