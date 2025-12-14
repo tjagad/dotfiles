7fc5e7247ab30ee9beb41f916594534cd00217c4
Tushar Jagad <tushar.jagad@gmail.com>
2025-12-14 17:23:20 +0530

    wezterm: switch to Maple Mono NFMono font
    Switch to Maple Mono NFMono font and remove using fallback
    configurations for font.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,7 +9,6 @@ config.font_size = 12
 -- config.font = wezterm.font('RecMonoLinear Nerd Font Mono', { weight = 'Light', italic = true })
 config.font = wezterm.font('FiraCode Nerd Font Mono Ret')
 config.font = wezterm.font('CodeNewRoman Nerd Font Mono')
-config.font = wezterm.font('Maple Mono NF')
 config.font = wezterm.font('MonacoLigaturized Nerd Font Mono')
 config.font = wezterm.font('RecMonoCasual Nerd Font Mono')
 config.font = wezterm.font('RecMonoLinear Nerd Font Mono')
@@ -18,8 +17,9 @@ config.font = wezterm.font('DroidSansM Nerd Font Mono')
 config.font = wezterm.font('VictorMono Nerd Font Mono', { weight = 'DemiBold' })
 config.font = wezterm.font('JuliaMono')
 config.font = wezterm.font('JetBrainsMono Nerd Font Mono', { weight = 'DemiBold', stretch = 'Normal' })
-config.font = wezterm.font_with_fallback {
-  { family = 'Maple Mono', weight = 'DemiBold', stretch = 'Normal' },
+config.font = wezterm.font('Maple Mono NFMono', { weight = 'Medium', stretch = 'Normal' })
+-- config.font = wezterm.font_with_fallback {
+--   { family = 'Maple Mono NFMono', weight = 'Medium', stretch = 'Normal' },
   -- { family = 'MonacoLigaturized Nerd Font Mono', weight = 'Regular' },
   -- { family = 'CodeNewRoman Nerd Font Mono', weight='Regular', stretch = 'Normal' },
   -- { family = 'RecMonoCasual Nerd Font Mono', weight='Regular', stretch = 'Normal' },
@@ -27,8 +27,8 @@ config.font = wezterm.font_with_fallback {
   -- { family = 'RecMonoLinear Nerd Font Mono', weight='Regular', stretch = 'Normal' },
   -- { family = 'VictorMono Nerd Font Mono', weight='DemiBold', stretch = 'Normal' },
   -- { family = 'JetBrainsMono Nerd Font Mono', weight='DemiBold', stretch = 'Normal' },
-  { family = 'Symbols Nerd Font Mono', weight = 'Regular', stretch = 'Normal' }
-}
+  -- { family = 'Symbols Nerd Font Mono', weight = 'Regular', stretch = 'Normal' }
+-- }
 config.enable_tab_bar = false
 
 config.colors = {
