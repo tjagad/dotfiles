1606e9e628c061506f8a3cfef33b42225cbf24cd
Tushar Jagad <tushar.jagad@gmail.com>
2026-03-06 22:26:46 +0530

    nvim: nvim-lazyvim-001: use copilot to update catppuccin config
    This commit update the catppuccin configurations to the latest version
    of the plugin.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- nvim-lazyvim-001/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,41 +1,72 @@
-  return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-      no_italic = true,
-      highlight_overrides = {
-        mocha = function(colors)
-          return {
-            WinSeparator = { fg = colors.surface1 },
-            -- use soft green, no italics for comments
-            Comment = { fg = colors.green, style = {} },
-            -- override default Pmenu colors to match overrides above
-            Pmenu = { fg = colors.text, bg = colors.base },
-            -- use brighter colors for relative numbers in the signcolumn
-            LineNrAbove = { fg = colors.overlay1 },
-            LineNr = { fg = colors.overlay1 },
-            LineNrBelow = { fg = colors.overlay1 },
-            -- use brighter colors for line numbers in the signcolumn
-            CursorLineNr = { fg = colors.mauve },
-          }
-        end,
-      },
-      integrations = {
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-        blink_cmp = {
-          enabled = true,
-          style = 'bordered',
-        },
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "mocha",
+    background = {
+      light = "latte",
+      dark = "mocha",
+    },
+    transparent_background = false,
+    show_end_of_buffer = false,
+    term_colors = false,
+    dim_inactive = {
+      enabled = false,
+      shade = "dark",
+      percentage = 0.15,
+    },
+    no_italic = true,
+    no_bold = false,
+    no_underline = false,
+    styles = {
+      comments = {},
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    color_overrides = {
+      mocha = {
+        base = "#000000",
+        mantle = "#000000",
+        crust = "#000000",
       },
     },
-  }
+    custom_highlights = function(colors)
+      return {
+        WinSeparator = { fg = colors.surface1 },
+        Comment = { fg = colors.green },
+        Pmenu = { fg = colors.text, bg = colors.base },
+        LineNrAbove = { fg = colors.overlay1 },
+        LineNr = { fg = colors.overlay1 },
+        LineNrBelow = { fg = colors.overlay1 },
+        CursorLineNr = { fg = colors.mauve },
+      }
+    end,
+    default_integrations = true,
+    integrations = {
+      blink_cmp = true,
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      dropbar = {
+        enabled = true,
+        color_mode = true,
+      },
+    },
+  },
+}
