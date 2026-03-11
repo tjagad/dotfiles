fe0c442d6487a31b08a45e5d332c5ea19aa61a9f
Tushar Jagad <tushar.jagad@gmail.com>
2026-03-11 14:47:08 +0530

    nvim: nvim-lazyvim-001: disable signature help for asm-lsp
    asm-lsp was throwing RPC errors related to signature help, which is not
    essential for assembly language development. Disabling this feature for
    asm-lsp should prevent these errors while still allowing the language
    server to function properly. This commit adds an on_attach function to the
    asm-lsp configuration to disable signature help.
    
    Signed-off-by: Tushar Jagad <tushar.jagad@gmail.com>

--- nvim-lazyvim-001/.config/nvim/lua/plugins/lsp.lua
+++ nvim-lazyvim-001/.config/nvim/lua/plugins/lsp.lua
@@ -15,6 +15,10 @@ return {
         cmd = { "asm-lsp" },
         filetypes = { "asm", "s", ".S" }, -- ensure .S files are recognized
         root_dir = require("lspconfig").util.root_pattern(".asm-lsp.toml"),
+        on_attach = function(client, bufnr)
+          -- Disable signature help for this server to stop the RPC errors
+          client.server_capabilities.signatureHelpProvider = false
+        end,
       },
     },
   },
